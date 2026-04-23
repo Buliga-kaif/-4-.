d120c75cd670fd4b3ef9502de30300eeb18ddc68
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2026-04-23 11:04:01 +0300

    Create задание 3.cpp

---
+++ задание 3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cmath>
+
+int main() {
+  
+    std::vector<int> V = { 12, -5, 34, 10, 25, 8, -13, 40, 7 };
+
+    std::map<int, std::vector<int>> M;
+
+    for (std::vector<int>::iterator it = V.begin(); it != V.end(); ++it) {
+     
+        int key = std::abs(*it) % 10;
+
+    
+        M[key].push_back(*it);
+    }
+   
+    for (std::map<int, std::vector<int>>::const_iterator it_m = M.begin();
+        it_m != M.end(); ++it_m) {
+
+        std::cout << "Ключ " << it_m->first << ": ";
+
+        for (std::vector<int>::const_iterator it_v = it_m->second.begin();
+            it_v != it_m->second.end(); ++it_v) {
+            std::cout << *it_v << " ";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
